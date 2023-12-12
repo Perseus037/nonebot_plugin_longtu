acc3f0e4af3c4288838af331e405e130e0d90a19
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2023-12-12 09:41:07 +0800

    Update __init__.py

--- nonebot-plugin-longtu/__init__.py
+++ nonebot-plugin-longtu/__init__.py
@@ -8,8 +8,7 @@ from nonebot.exception import FinishedException
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 from nonebot.plugin import PluginMetadata
 
-dragon = on_command("dragon", aliases={"龙龙", "龙图"}, priority=5)
-
+__version__ = "0.1.0.post1"
 __plugin_meta__ = PluginMetadata(
     name = "龙神",
     description = "发送dragon，龙龙或者龙图随即发送一张可爱龙龙",
@@ -18,6 +17,8 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/Perseus037/nonebot_plugin_longtu",
     supported_adapters = {"nonebot.adapters.onebot.v11"},
 )
+
+dragon = on_command("dragon", aliases={"龙龙", "龙图"}, priority=5)
 
 @dragon.handle()
 async def handle_first_receive(bot: Bot, event: MessageEvent):
@@ -28,23 +29,29 @@ async def handle_first_receive(bot: Bot, event: MessageEvent):
 
     if batch_choice == 'batch1/':
         selected_image_number = random.randint(1, 500)
+        
     elif batch_choice == 'batch2/':
         selected_image_number = random.randint(501, 1000)
+        
     else:
         selected_image_number = random.randint(1001, 1516)
 
     for ext in extensions:
+        
         image_url = f"{base_url}{batch_choice}dragon_{selected_image_number}_{ext}"
         try:
             async with AsyncClient() as client:
                 resp = await client.get(image_url, timeout=5.0)
+                
             if resp.status_code == 200:
                 picbytes = BytesIO(resp.content).getvalue()
                 await dragon.finish(MessageSegment.image(picbytes))
                 break
+                
         except Exception as e:
             if isinstance(e, FinishedException):
                 pass
+                
             else:
                 print(f"尝试发送图片时出现异常：{e}")
                 if ext == extensions[-1]:
