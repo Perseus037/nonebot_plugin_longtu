d2da6a425785bdf3ecf109cc91fc6a6ca1baf351
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2024-01-12 08:47:31 +0800

    Update __init__.py

--- nonebot_plugin_longtu/__init__.py
+++ nonebot_plugin_longtu/__init__.py
@@ -1,22 +1,21 @@
 import random
-
 from httpx import AsyncClient, ReadTimeout, ConnectError
 from io import BytesIO
-
 from nonebot import on_command
 from nonebot.exception import FinishedException
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 from nonebot.plugin import PluginMetadata
 from nonebot.log import logger
+from .config import Config
 
-__version__ = "0.1.0.post4"
+__version__ = "0.1.1.post1"
 __plugin_meta__ = PluginMetadata(
     name="随机龙图",
     description="2024年是龙年...我都准备好了",
     usage="使用命令：龙龙，龙图，dragon [数量]",
     homepage="https://github.com/Perseus037/nonebot_plugin_longtu",
     type="application",
-    config=None,
+    config=Config,
     supported_adapters={"~onebot.v11"},
 )
 
@@ -24,13 +23,17 @@ dragon = on_command("dragon", aliases={"龙龙", "龙图"}, priority=5)
 
 @dragon.handle()
 async def handle_first_receive(bot: Bot, event: MessageEvent):
-    message_text = event.get_plaintext().strip() 
-    args = message_text.split()  
-    num_dragons = 1  # 默认发送1张龙图
-
+    logger.info(f"最大图片数量：{config.max_dragons}")
+    message_text = event.get_plaintext().strip()  
+    args = message_text.split() 
+    num_dragons = 1
+    
     if len(args) > 1 and args[1].isdigit():
-        num_dragons = int(args[1])
-        num_dragons = min(num_dragons, 5)  # 限制最多发送5张图片
+        requested_num_dragons = int(args[1])
+        if requested_num_dragons > config.max_dragons:
+            await bot.send(event, f"无法发送超过 {config.max_dragons} 张图片。")
+            return
+        num_dragons = requested_num_dragons
 
     base_url = "https://git.acwing.com/Est/dragon/-/raw/main/"
     extensions = ['.jpg', '.png', '.gif']
@@ -38,13 +41,11 @@ async def handle_first_receive(bot: Bot, event: MessageEvent):
     for _ in range(num_dragons):
         batch_choice = random.choice(['batch1/', 'batch2/', 'batch3/'])
         if batch_choice == 'batch1/':
-         selected_image_number = random.randint(1, 500)
-        
+            selected_image_number = random.randint(1, 500)
         elif batch_choice == 'batch2/':
-         selected_image_number = random.randint(501, 1000)
-        
+            selected_image_number = random.randint(501, 1000)
         else:
-         selected_image_number = random.randint(1001, 1516)
+            selected_image_number = random.randint(1001, 1516)
 
         for ext in extensions:
             image_url = f"{base_url}{batch_choice}dragon_{selected_image_number}_{ext}"
